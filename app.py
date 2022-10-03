cfd3288bbeff2bd3e5309af3608e9ffb8e4753ba
Cevher <cevheryilmaz96@outlook.com>
2022-10-03 21:36:26 +0200

    the file-upload function created

--- app.py
+++ app.py
@@ -6,20 +6,28 @@ Created on Sun Oct  2 22:14:00 2022
 """
 
 # Importing Necessary modules
-from fastapi import FastAPI
+from fastapi import FastAPI,File,UploadFile
 import uvicorn
  
 # Declaring our FastAPI instance
 app = FastAPI()
  
-# Defining path operation for root endpoint
-@app.get('/')
-def main():
-    return {'message': 'Get request!'}
- 
-# Defining path operation for /name endpoint
-@app.get('/{name}')
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
 def hello_name(name : str):
     # Defining a function that takes only string as input and output the
     # following message.
     return {'message': f'Welcome to here!, {name}'}
+
+if __name__=='__main__':
+    uvicorn.run(app,host="127.0.0.1",port="8000")
